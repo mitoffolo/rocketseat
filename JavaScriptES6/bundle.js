11c0881b27bbd4695c81fd31e61a52ab8418c99d
unknown <dit.michel@gmaiil.com>
2020-01-15 22:28:14 -0300

    Operações em Array

--- JavaScriptES6/bundle.js
+++ JavaScriptES6/bundle.js
@@ -1,7 +1,27 @@
 "use strict";
 
-var usuario = [{
-  nome: "Pedro"
-}];
-usuario.nome = "Pedro";
-console.log(usuario.nome);
+var arr = [1, 3, 4, 5, 8, 9];
+/*Percorre o array e multiplica função MAP*/
+
+var newArr = arr.map(function (item, idex) {
+  return item * 2;
+});
+console.log(newArr);
+/*Percorre todos os valores e soma eles ou faz outra coisa*/
+
+var sun = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sun);
+/*Retorna todos os pares, o restur sempre retornará o true.*/
+
+var pares = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+/*Procura o valor 4 no array*/
+
+var procura = arr.find(function (item) {
+  return item === 4;
+});
+console.log(procura);
+console.log(pares);
--- JavaScriptES6/main.js
+++ JavaScriptES6/main.js
@@ -1,5 +1,48 @@
-const usuario = [{nome: "Pedro"}];
+const arr = [1,3,4,5,8,9];
 
-usuario.nome = "Pedro";
+/*Percorre o array e multiplica função MAP*/ 
 
-console.log(usuario.nome);
+const newArr = arr.map(function(item,idex){
+
+    return item * 2;
+
+
+});
+
+console.log(newArr);
+
+/*Percorre todos os valores e soma eles ou faz outra coisa*/ 
+
+const sun = arr.reduce(function(total,next){
+
+    return total + next;
+
+
+});
+
+console.log(sun);
+
+/*Retorna todos os pares, o restur sempre retornará o true.*/
+const pares = arr.filter(function(item){
+
+
+    return item % 2 === 0;
+
+});
+
+
+/*Procura o valor 4 no array*/ 
+
+const procura = arr.find(function(item){
+
+    return item === 4;
+
+});
+
+console.log(procura);
+
+
+
+
+
+console.log(pares);
