5bb2c0e9315a436457e0efe4bab0718a5d6d20e5
unknown <dit.michel@gmaiil.com>
2020-01-17 20:42:55 -0300

    Arrow Functions

--- JavaScriptES6/bundle.js
+++ JavaScriptES6/bundle.js
@@ -3,25 +3,20 @@
 var arr = [1, 3, 4, 5, 8, 9];
 /*Percorre o array e multiplica função MAP*/
 
-var newArr = arr.map(function (item, idex) {
+var newArr = arr.map(function (item) {
   return item * 2;
 });
 console.log(newArr);
-/*Percorre todos os valores e soma eles ou faz outra coisa*/
 
-var sun = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sun);
-/*Retorna todos os pares, o restur sempre retornará o true.*/
+var teste = function teste() {
+  return 'teste';
+};
 
-var pares = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-/*Procura o valor 4 no array*/
+var obj = function obj() {
+  return {
+    nome: 'michel'
+  };
+};
 
-var procura = arr.find(function (item) {
-  return item === 4;
-});
-console.log(procura);
-console.log(pares);
+console.log(teste());
+console.log(obj());
--- JavaScriptES6/main.js
+++ JavaScriptES6/main.js
@@ -2,47 +2,15 @@ const arr = [1,3,4,5,8,9];
 
 /*Percorre o array e multiplica função MAP*/ 
 
-const newArr = arr.map(function(item,idex){
-
-    return item * 2;
-
-
-});
+const newArr = arr.map(item => item * 2);
 
 console.log(newArr);
 
-/*Percorre todos os valores e soma eles ou faz outra coisa*/ 
 
-const sun = arr.reduce(function(total,next){
+const teste = () => {return 'teste'};
 
-    return total + next;
-
-
-});
-
-console.log(sun);
-
-/*Retorna todos os pares, o restur sempre retornará o true.*/
-const pares = arr.filter(function(item){
+const obj = () => ({nome: 'michel'});
 
 
-    return item % 2 === 0;
-
-});
-
-
-/*Procura o valor 4 no array*/ 
-
-const procura = arr.find(function(item){
-
-    return item === 4;
-
-});
-
-console.log(procura);
-
-
-
-
-
-console.log(pares);
+console.log(teste());
+console.log(obj());
